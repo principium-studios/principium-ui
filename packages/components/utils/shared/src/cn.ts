aad40f332627b2960024b987ec0d3d7690a82e68
Covei Rares <rarescovei5@gmail.com>
2025-07-14 23:32:30 +0300

    fixed twMerge merging my text-tiny with text colors

--- packages/components/utils/shared/src/cn.ts
+++ packages/components/utils/shared/src/cn.ts
@@ -1,4 +1,12 @@
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
 import { clsx, type ClassValue } from "clsx";
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": ["text-tiny"],
+    },
+  },
+});
 
 export const cn = (...args: ClassValue[]) => twMerge(clsx(args));
