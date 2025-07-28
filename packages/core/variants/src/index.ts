56df669d11a4115dd9094ad3d04abdc853deef25
Covei Rares <rarescovei5@gmail.com>
2025-07-28 18:45:13 +0300

    actually the issue is this which is even worse

--- packages/core/variants/src/index.ts
+++ packages/core/variants/src/index.ts
@@ -1,31 +1,36 @@
-// import {pv} from './pv';
-// import {CompoundVariants, SlotIsInCompoundOption} from './types';
+import {pv} from './pv';
+import {SlotIsInCompoundOption} from './types';
 
 export {pv, createPv} from './pv';
 export type SlotParams<T> = T extends (props: infer P) => any ? P : never;
 
-// Why teh fuk does this suggest disableAnimation ????????????????????
-// For some reason the class type when ifnered it is widened to contai nthe tabList so it automatically sees it as
-// beeing apart of the class ??????????
-// const slots = {
-//   tabList: '',
-// };
-// const variants = {
-//   disableAnimation: {
-//     true: {},
-//   },
-// };
-// const compoundVariants: CompoundVariants<typeof slots, typeof variants> = [
-//   {
-//     disableAnimation: true,
-//     class: {},
-//   },
-// ];
+const tabsVariants = pv(
+  {
+    tabList: [],
+  },
+  {
+    variants: {
+      variant: {
+        underlined: {},
+      },
+      disableAnimation: {
+        true: {},
+      },
+    },
+    compoundVariants: [
+      {
+        disableAnimation: true,
+        class: {},
+      },
+      // when deleting this compound variant disableanimaation is not suggested anymore
+      {
+        variant: 'underlined',
+        class: {
+          tabList: [],
+        },
+      },
+    ],
+  },
+);
 
-// type typeDisable = SlotIsInCompoundOption<
-//   typeof slots,
-//   typeof variants,
-//   typeof compoundVariants,
-//   'tabList',
-//   'disableAnimation'
-// >;
+tabsVariants.tabList({});
